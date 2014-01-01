5a0fb3ed7de876010c76c402b48a3bf8c2634c26
Admin <admin@Admins-Mac-Pro.local>
2014-01-01 18:04:42 -0500

    Removed refs to imageuploader and pixelpress.

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var connect = require('connect'),
     fs = require('fs'),
-    io = require('socket.io').listen(1337),
-    FSUpload = require('fsupload').FSUpload,
-    PixelPress = require('pixelpress').PixelPress;
+    io = require('socket.io').listen(1337);
 
 connect(connect.static(__dirname + '/public')).listen(8000);
 
