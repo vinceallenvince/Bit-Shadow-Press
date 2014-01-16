015786e0bee7cd42eb37dd7a4da12af36a748e9e
Foldi <vince@foldifoldi.com>
2014-01-16 15:18:57 -0500

    Adds Johnny-five potentiometer.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var connect = require('connect'),
     fs = require('fs'),
-    io = require('socket.io').listen(1337);
+    io = require('socket.io').listen(1337),
+    EventEmitter = require('events').EventEmitter;
+
+var emitter = new EventEmitter();
 
 connect(connect.static(__dirname + '/public')).listen(8000);
 
@@ -23,4 +26,37 @@ io.sockets.on('connection', function(socket) {
       socket.emit('dataSet', data);
     });
   });
+  
+  // control frame rate slider
+  emitter.on('potFrameRate', function(data) {
+    socket.emit('potFrameRate', data);
+  });
+
+});
+
+//
+
+var five = require('johnny-five'),
+    board = new five.Board();
+
+board.on('ready', function() {
+
+   // Create a new `potentiometer` hardware instance.
+  potentiometer = new five.Sensor({
+    pin: 'A2',
+    freq: 60
+  });
+
+  // Inject the `sensor` hardware into
+  // the Repl instance's context;
+  // allows direct command line access
+  board.repl.inject({
+    pot: potentiometer
+  });
+
+  // "data" get the current reading from the potentiometer
+  potentiometer.on('data', function() {
+    emitter.emit('potFrameRate', this.value);
+  });
+
 });
